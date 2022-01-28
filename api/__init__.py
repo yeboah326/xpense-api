5763a7e394f83adc7a8711ba92c12eeac50b5ce5
Gideon Yeboah Asante <asanteg36@gmail.com>
2022-01-28 18:03:16 +0000

    Register the main blueprints in the main app

--- api/__init__.py
+++ api/__init__.py
@@ -1,17 +1,24 @@
-from flask import Flask
+from apiflask import APIFlask
 from .extensions import *
 from dotenv import load_dotenv
+from .user.controllers import user
+from .expense.controllers import expense
 
 # Load environment variables
 load_dotenv()
 
+
 def create_app():
-    app = Flask(__name__)
-    
+    app = APIFlask(__name__, title="xpense API", version="1.0.0")
+
     # Flask application extensions
     db.init_app(app)
     cors.init_app(app)
     jwt.init_app(app)
     migrate.init_app(app, db)
+
+    # Register blueprints
+    app.register_blueprint(user)
+    app.register_blueprint(expense)
 
     return app

59ba79c5fa198c4ef6adad5f73212c97bfdfea10
Gideon Yeboah Asante <asanteg36@gmail.com>
2022-02-01 18:48:05 +0000

    Add faker to the extensions and load environment variables in the test configuration file

--- api/config.py
+++ api/config.py
@@ -1,7 +1,12 @@
 import os
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
 
 
 class BaseConfig(object):
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
     JWT_ERROR_MESSAGE_KEY = os.getenv("JWT_ERROR_MESSAGE_KEY")
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- api/extensions.py
+++ api/extensions.py
@@ -2,8 +2,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
+from faker import Faker
 
 db = SQLAlchemy()
 cors = CORS()
 jwt = JWTManager()
 migrate = Migrate()
+fake = Faker()

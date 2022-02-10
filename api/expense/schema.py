6adb2b8cbbfe9f53d0e027acc66af244546e11e4
Gideon Yeboah Asante <asanteg36@gmail.com>
2022-02-10 09:37:57 +0000

    Made some changes to make it easier to display data on frontend

--- api/expense/schema.py
+++ api/expense/schema.py
@@ -1,5 +1,5 @@
 from apiflask import Schema
-from apiflask.fields import Date, String, Integer, Float, List, Nested
+from apiflask.fields import Date, String, Integer, Float, List, Nested, Dict
 
 
 class ExpenseLoad(Schema):
@@ -15,12 +15,21 @@ class ExpenseDump(Schema):
     date = Date(required=True)
     user_id = Integer(required=True)
 
+
 class ExpenseSummary(Schema):
     today = Float(required=True)
     seven_days = Float(required=True)
     thirty_days = Float(required=True)
     sixty_days = Float(required=True)
 
+
 class ExpenseMonthSumary(Schema):
-    expenses = List(Nested(ExpenseDump),required=True)
+    expenses = List(Dict(keys=String, values=List(Nested(ExpenseDump), required=True), required=True))
     expenses_total_sum = Float(required=True)
+    month = String(required=True)
+    month_index = Integer(required=True)
+    year = Integer(required=True)
+
+
+class ExpenseDay(Schema):
+    day = Integer(required=True)
--- api/expense/utils.py
+++ api/expense/utils.py
@@ -1,3 +1,4 @@
+from calendar import month
 from datetime import datetime, timedelta
 from api.expense.models import Expense
 from api.extensions import db
@@ -37,14 +38,42 @@ def generate_user_month_summary(user_id, index):
     # Generate the date for today
     today = datetime.today()
 
-    expenses = db.session.query(Expense).filter(
-        Expense.user_id == user_id).filter(
-            extract("month", Expense.date) == index).filter(
-                extract("year", Expense.date) == today.year).all()
+    # List to store the expenses according to days
+    expenses_list = []
+
+    for i in range(1, 32):
+        expenses = db.session.query(Expense).filter(
+            Expense.user_id == user_id).filter(
+                extract("month", Expense.date) == index).filter(
+                    extract("year", Expense.date) == today.year).filter(
+                        extract("day", Expense.date) == i).all()
+        if expenses:
+            expenses_list.append({f"{i}": expenses})
 
     expenses_total_sum = db.session.query(
         func.sum(Expense.amount).filter(Expense.user_id == user_id).filter(
             extract("month", Expense.date) == index).filter(
                 extract("year", Expense.date) == today.year)).first()[0]
 
-    return {"expenses": expenses, "expenses_total_sum": expenses_total_sum}
+    months = {
+        1: "january",
+        2: "february",
+        3: "march",
+        4: "april",
+        5: "may",
+        6: "june",
+        7: "july",
+        8: "august",
+        9: "september",
+        10: "october",
+        11: "november",
+        12: "december",
+    }
+
+    return {
+        "expenses": expenses_list,
+        "expenses_total_sum": expenses_total_sum,
+        'month': months[index],
+        'month_index': index,
+        'year': today.year
+    }

6d491ee0b2a9e3536a847ebae8e12e898a9f217e
Gideon Yeboah Asante <asanteg36@gmail.com>
2022-02-11 18:09:43 +0000

    Add new config

--- api/config.py
+++ api/config.py
@@ -1,12 +1,14 @@
 import os
 from dotenv import load_dotenv
+import datetime
 
 # Load environment variables
 load_dotenv()
 
 
 class BaseConfig(object):
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+    JWT_ACCESS_TOKEN_EXPIRES=datetime.timedelta(seconds=3)
     JWT_ERROR_MESSAGE_KEY = os.getenv("JWT_ERROR_MESSAGE_KEY")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False

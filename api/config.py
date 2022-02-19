1955048b922b7904192bd03a45075737ba3b566c
Gideon Yeboah Asante <asanteg36@gmail.com>
2022-02-19 08:18:46 +0000

    Taking care of SQLAlchemy 1.4+ compatibility issues on heroku

--- api/config.py
+++ api/config.py
@@ -7,8 +7,11 @@ load_dotenv()
 
 
 class BaseConfig(object):
-    JWT_ACCESS_TOKEN_EXPIRES=datetime.timedelta(hours=3)
+    JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(hours=3)
     JWT_ERROR_MESSAGE_KEY = os.getenv("JWT_ERROR_MESSAGE_KEY")
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace(
+            "postgres://", "postgresql://", 1)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
